f5dbcb6d394f1cc169387b7aa3d4d919e11bcfdf
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-08-04 01:25:50 +0330

    improve resourceHelper

---
+++ src/Core/Tools/Helpers/ResourceHelper/IResourceHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Windows.ApplicationModel.Resources;
+
+namespace WinUICommunity;
+public interface IResourceHelper
+{
+    ResourceManager ResourceManager { get; set; }
+    ResourceContext ResourceContext { get; set; }
+
+    void Initialize();
+    void Initialize(ResourceManager resourceManager);
+    void Initialize(ResourceManager resourceManager, ResourceContext resourceContext);
+    List<string> GetAllResourcesKeys(string identifier = null);
+    string GetString(string key, string filename = "Resources");
+}
---
+++ src/Core/Tools/Helpers/ResourceHelper/ResourceHelper.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Windows.ApplicationModel.Resources;
+
+namespace WinUICommunity;
+
+public class ResourceHelper : IResourceHelper
+{
+    public ResourceManager ResourceManager { get; set; }
+    public ResourceContext ResourceContext { get; set; }
+
+    public ResourceHelper()
+    {
+        Initialize();
+    }
+
+    public ResourceHelper(ResourceManager resourceManager)
+    {
+        Initialize(resourceManager);
+    }
+
+    public ResourceHelper(ResourceManager resourceManager, ResourceContext resourceContext)
+    {
+        Initialize(resourceManager, resourceContext);
+    }
+
+    public void Initialize()
+    {
+        ResourceManager = new ResourceManager();
+        ResourceContext = ResourceManager.CreateResourceContext();
+    }
+
+    public void Initialize(ResourceManager resourceManager)
+    {
+        this.ResourceManager = resourceManager;
+        this.ResourceContext = resourceManager.CreateResourceContext();
+    }
+
+    public void Initialize(ResourceManager resourceManager, ResourceContext resourceContext)
+    {
+        this.ResourceManager = resourceManager;
+        this.ResourceContext = resourceContext;
+    }
+    
+    /// <summary>
+    /// Get All Resources Keys
+    /// </summary>
+    /// <param name="identifier">Default value is Resources</param>
+    /// <returns></returns>
+    public List<string> GetAllResourcesKeys(string identifier = null)
+    {
+        var reslist = new List<string>();
+
+        var rmap = Windows.ApplicationModel.Resources.Core.ResourceManager.Current.MainResourceMap;
+        foreach (var str in rmap.Keys)
+        {
+            if (str.StartsWith(identifier ?? "Resources"))
+            {
+                reslist.Add($"/{str}");
+            }
+        }
+
+        return reslist;
+    }
+
+    public void SetLanguage(string language)
+    {
+        ResourceContext.QualifierValues["Language"] = language;
+        if (ApplicationHelper.IsPackaged)
+        {
+            Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = language;
+        }
+    }
+
+    public string GetString(string key, string filename = "Resources")
+    {
+        var candidate = ResourceManager.MainResourceMap.TryGetValue($"{filename}/{key}", ResourceContext);
+        return candidate != null ? candidate.ValueAsString : key;
+    }
+}
