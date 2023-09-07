12752fec6fbd8532fa85cfdc3073cc7954f91362
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-09-07 16:38:01 +0330

    Create AcrylicBackdropHelper.cs

---
+++ dev/Core/Tools/ThemeService/AcrylicBackdropHelper.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.UI.Composition.SystemBackdrops;
+using WinRT;
+
+namespace WinUICommunity;
+
+public class WindowsSystemDispatcherQueueHelper
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct DispatcherQueueOptions
+    {
+        internal int dwSize;
+        internal int threadType;
+        internal int apartmentType;
+    }
+
+    [DllImport("CoreMessaging.dll")]
+    private static extern int CreateDispatcherQueueController([In] DispatcherQueueOptions options, [In, Out, MarshalAs(UnmanagedType.IUnknown)] ref object dispatcherQueueController);
+
+    object m_dispatcherQueueController = null;
+    public void EnsureWindowsSystemDispatcherQueueController()
+    {
+        if (Windows.System.DispatcherQueue.GetForCurrentThread() != null)
+        {
+            // one already exists, so we'll just use it.
+            return;
+        }
+
+        if (m_dispatcherQueueController == null)
+        {
+            DispatcherQueueOptions options;
+            options.dwSize = Marshal.SizeOf(typeof(DispatcherQueueOptions));
+            options.threadType = 2;    // DQTYPE_THREAD_CURRENT
+            options.apartmentType = 2; // DQTAT_COM_STA
+
+            CreateDispatcherQueueController(options, ref m_dispatcherQueueController);
+        }
+    }
+}
+
+public class AcrylicBackdropHelper
+{
+    public Window window;
+    public WindowsSystemDispatcherQueueHelper m_wsdqHelper;
+    public DesktopAcrylicController m_acrylicController;
+    public SystemBackdropConfiguration m_configurationSource;
+
+    public AcrylicBackdropHelper(Window window)
+    {
+        this.window = window;
+    }
+
+    public DesktopAcrylicKind GetDesktopAcrylicKind()
+    {
+        return m_acrylicController.Kind;
+    }
+
+    public bool TrySetAcrylicBackdrop(DesktopAcrylicKind acrylicKind)
+    {
+        if (DesktopAcrylicController.IsSupported())
+        {
+            m_wsdqHelper = new WindowsSystemDispatcherQueueHelper();
+            m_wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
+
+            // Hooking up the policy object
+            m_configurationSource = new SystemBackdropConfiguration();
+            window.Activated += Window_Activated;
+            window.Closed += Window_Closed;
+            ((FrameworkElement)window.Content).ActualThemeChanged += Window_ThemeChanged;
+
+            // Initial configuration state.
+            m_configurationSource.IsInputActive = true;
+            SetConfigurationSourceTheme();
+
+            m_acrylicController = new DesktopAcrylicController();
+            m_acrylicController.Kind = acrylicKind;
+
+            // Enable the system backdrop.
+            // Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
+            m_acrylicController.AddSystemBackdropTarget(window.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
+            m_acrylicController.SetSystemBackdropConfiguration(m_configurationSource);
+            return true; // succeeded
+        }
+
+        return false; // Acrylic is not supported on this system
+    }
+
+    private void Window_Activated(object sender, WindowActivatedEventArgs args)
+    {
+        m_configurationSource.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
+    }
+
+    private void Window_Closed(object sender, WindowEventArgs args)
+    {
+        // Make sure any Mica/Acrylic controller is disposed so it doesn't try to
+        // use this closed window.
+        if (m_acrylicController != null)
+        {
+            m_acrylicController.Dispose();
+            m_acrylicController = null;
+        }
+        window.Activated -= Window_Activated;
+        m_configurationSource = null;
+    }
+
+    private void Window_ThemeChanged(FrameworkElement sender, object args)
+    {
+        if (m_configurationSource != null)
+        {
+            SetConfigurationSourceTheme();
+        }
+    }
+
+    private void SetConfigurationSourceTheme()
+    {
+        switch (((FrameworkElement)window.Content).ActualTheme)
+        {
+            case ElementTheme.Dark: m_configurationSource.Theme = SystemBackdropTheme.Dark; break;
+            case ElementTheme.Light: m_configurationSource.Theme = SystemBackdropTheme.Light; break;
+            case ElementTheme.Default: m_configurationSource.Theme = SystemBackdropTheme.Default; break;
+        }
+    }
+}
