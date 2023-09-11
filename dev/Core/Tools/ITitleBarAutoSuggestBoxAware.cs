72b66f0e827094a72c61ab7d2d43f7c294099db7
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-09-11 17:44:49 +0330

    add ITitleBarAutoSuggestBoxAware interface

---
+++ dev/Core/Tools/ITitleBarAutoSuggestBoxAware.cs
@@ -0,0 +1,6 @@
+﻿namespace WinUICommunity.Core.Tools;
+public interface ITitleBarAutoSuggestBoxAware
+{
+    void OnAutoSuggestBoxTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args);
+    void OnAutoSuggestBoxQuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args);
+}
