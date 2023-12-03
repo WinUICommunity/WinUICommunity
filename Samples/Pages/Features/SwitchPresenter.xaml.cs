dab10bc48d7c5d804a1c07587ee36dfd4cb899a7
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-12-03 20:10:40 +0330

    Add More Demo

---
+++ Samples/Pages/Features/SwitchPresenter.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace WinUICommunity.DemoApp.Pages;
+public sealed partial class SwitchPresenter : Page
+{
+    public SwitchPresenter()
+    {
+        this.InitializeComponent();
+    }
+}
+
+public enum Animal
+{
+    Cat,
+    Dog,
+    Bunny,
+    Llama,
+    Parrot,
+    Squirrel
+}
---
+++ Samples/Pages/Features/ValidationPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace WinUICommunity.DemoApp.Pages;
+public sealed partial class ValidationPage : Page
+{
+    public ValidationPage()
+    {
+        this.InitializeComponent();
+    }
+}
