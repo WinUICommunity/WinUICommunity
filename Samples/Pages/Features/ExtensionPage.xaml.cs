5d30db647306fcc732c62184bece74c11ebcfdf2
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-12-03 20:47:14 +0330

    Add ExtensionPage

---
+++ Samples/Pages/Features/ExtensionPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace WinUICommunity.DemoApp.Pages;
+
+public sealed partial class ExtensionPage : Page
+{
+    public ExtensionPage()
+    {
+        this.InitializeComponent();
+    }
+}
