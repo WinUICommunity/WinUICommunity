962275a02fedd43b9fad8fa08c2408ed4368009a
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-12-03 19:43:47 +0330

    Add OptionsPageControlPage Demo

---
+++ Samples/Pages/Features/OptionsPageControlPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.UI.Xaml.Controls;
+
+namespace WinUICommunity.DemoApp.Pages;
+public sealed partial class OptionsPageControlPage : Page
+{
+    public OptionsPageControlPage()
+    {
+        this.InitializeComponent();
+    }
+
+    private void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        OptionsPage.OptionsBarMinWidth = 48.0;
+    }
+}
