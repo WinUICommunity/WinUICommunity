13485bfce03b1044e5588aad25941f63adf42173
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-12-03 19:50:35 +0330

    Add LoadingPage Demo

---
+++ Samples/Pages/Features/LoadingPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace WinUICommunity.DemoApp.Pages;
+public sealed partial class LoadingPage : Page
+{
+    public LoadingPage()
+    {
+        this.InitializeComponent();
+    }
+}
