e1afef283a389b5313f7c1b4d4af99ca4ecde177
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-12-07 13:17:43 +0330

    Add More Demo

---
+++ Samples/Pages/Features/FileAndFolderPickerPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using DemoApp;
+using Microsoft.UI.Xaml.Controls;
+
+namespace WinUICommunity.DemoApp.Pages;
+public sealed partial class FileAndFolderPickerPage : Page
+{
+    public FileAndFolderPickerPage()
+    {
+        this.InitializeComponent();
+    }
+
+    private async void btnPickSaveFileAsync_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        var ext = new Dictionary<string, IList<string>>();
+        ext.Add("Plain Text", new List<string>() { ".txt" });
+        var picker = await FileAndFolderPickerHelper.PickSaveFileAsync(App.currentWindow, ext);
+        if (picker != null)
+        {
+            txtRes1.Text = picker.Path;
+        }
+    }
+
+    private async void btnPickMultipleFilesAsync_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        var fileTypeFilter = new List<string> { ".txt", ".rtf" };
+        var picker = await FileAndFolderPickerHelper.PickMultipleFilesAsync(App.currentWindow, fileTypeFilter);
+        if (picker != null)
+        {
+            foreach (var item in picker)
+            {
+                txtRes2.Text = item.Path;
+            }
+        }
+    }
+
+    private async void btnPickSingleFileAsync_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        var fileTypeFilter = new List<string> { ".txt", ".rtf" };
+        var picker = await FileAndFolderPickerHelper.PickSingleFileAsync(App.currentWindow, fileTypeFilter);
+        if (picker != null)
+        {
+            txtRes3.Text = picker.Path;
+        }
+    }
+
+    private async void btnPickSingleFolderAsync_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        var picker = await FileAndFolderPickerHelper.PickSingleFolderAsync(App.currentWindow);
+        if (picker != null)
+        {
+            txtRes4.Text = picker.Path;
+        }
+    }
+}
