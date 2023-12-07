37fa9be16a7ae94775695fd1da10e0a335336e05
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-12-07 11:41:11 +0330

    Update OptionsPageControlPage.xaml.cs

--- Samples/Pages/Features/OptionsPageControlPage.xaml.cs
+++ Samples/Pages/Features/OptionsPageControlPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml.Controls;
 
 namespace WinUICommunity.DemoApp.Pages;
 public sealed partial class OptionsPageControlPage : Page
@@ -7,10 +6,5 @@ public sealed partial class OptionsPageControlPage : Page
     public OptionsPageControlPage()
     {
         this.InitializeComponent();
-    }
-
-    private void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-    {
-        OptionsPage.OptionsBarMinWidth = 48.0;
     }
 }
