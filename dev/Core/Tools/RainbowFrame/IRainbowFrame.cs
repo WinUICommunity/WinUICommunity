d7b143b9b65390a957e9e334afd42d29565f1de3
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-12-08 18:36:10 +0330

    Update IRainbowFrame.cs

--- dev/Core/Tools/RainbowFrame/IRainbowFrame.cs
+++ dev/Core/Tools/RainbowFrame/IRainbowFrame.cs
@@ -1,9 +1,12 @@
 ﻿namespace WinUICommunity;
 public interface IRainbowFrame
 {
+    void Initialize(IntPtr window, TimeSpan frameUpdateInterval, int effectSpeed);
     void Initialize(Window window, TimeSpan frameUpdateInterval, int effectSpeed);
     void Initialize(Window window, TimeSpan frameUpdateInterval);
+    void Initialize(IntPtr window, TimeSpan frameUpdateInterval);
     void Initialize(Window window, int effectSpeed);
+    void Initialize(IntPtr window);
     void Initialize(Window window);
     void UpdateEffectSpeed(int effectSpeed);
     void ResetFrameColorToDefault();
