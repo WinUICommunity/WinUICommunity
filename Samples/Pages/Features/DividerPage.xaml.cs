69445d6af3f6557f82a4afca42839bf7e55c7b98
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-12-24 23:35:24 +0330

    Add Divider Demo

---
+++ Samples/Pages/Features/DividerPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace WinUICommunity.DemoApp.Pages;
+public sealed partial class DividerPage : Page
+{
+    public DividerPage()
+    {
+        this.InitializeComponent();
+    }
+}
