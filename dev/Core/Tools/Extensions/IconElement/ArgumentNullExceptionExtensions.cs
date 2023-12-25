e45e36209278cd40752cecb4438cf5e6b5a4f249
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-12-25 20:28:52 +0330

    Create ArgumentNullExceptionExtensions.cs

---
+++ dev/Core/Tools/Extensions/IconElement/ArgumentNullExceptionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace WinUICommunity;
+internal static class ArgumentNullExceptionExtensions
+{
+    [DoesNotReturn]
+    public static void Throw(this ArgumentNullException? _, string? parameterName)
+    {
+        throw new ArgumentNullException(parameterName);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfNull(this ArgumentNullException? _, [NotNull] object? argument, [CallerArgumentExpression(nameof(argument))] string? parameterName = null)
+    {
+        if (argument is null)
+        {
+            Throw(parameterName);
+        }
+    }
+
+    [DoesNotReturn]
+    private static void Throw(string? parameterName)
+    {
+        throw new ArgumentNullException(parameterName);
+    }
+}
