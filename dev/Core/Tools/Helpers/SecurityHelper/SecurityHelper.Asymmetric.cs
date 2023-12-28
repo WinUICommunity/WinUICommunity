a51f2df4dd01cb2c94b6504f02c684244aa357a6
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-12-28 18:04:35 +0330

    Update SecurityHelper.Asymmetric.cs

--- dev/Core/Tools/Helpers/SecurityHelper/SecurityHelper.Asymmetric.cs
+++ dev/Core/Tools/Helpers/SecurityHelper/SecurityHelper.Asymmetric.cs
@@ -5,17 +5,21 @@ using Windows.Storage.Streams;
 namespace WinUICommunity;
 public static partial class SecurityHelper
 {
-    private static string EncryptStringAsymmetricBase(string plainText, string publicKey, IBuffer publicKeyBuffer, AsymmetricAlgorithm asymmetricAlgorithm, EncodeType encodeType)
+    private static string CryptoStringAsymmetricBase(CryptoMode cryptoMode, string value, string publicOrPrivateKey, IBuffer publicOrPrivateKeyBuffer, AsymmetricAlgorithm asymmetricAlgorithm, EncodeType encodeType)
     {
-        IBuffer keyBuffer = CryptographicBuffer.ConvertStringToBinary(plainText, BinaryStringEncoding.Utf8);
+        var keyBuffer = cryptoMode == CryptoMode.Encrypt
+            ? CryptographicBuffer.ConvertStringToBinary(value, BinaryStringEncoding.Utf8)
+            : encodeType == EncodeType.Hex
+        ? CryptographicBuffer.DecodeFromHexString(value)
+        : CryptographicBuffer.DecodeFromBase64String(value);
 
         CryptographicKey key;
 
-        if (publicKeyBuffer == null)
+        if (publicOrPrivateKeyBuffer == null)
         {
             var keyBlob = encodeType == EncodeType.Hex
-                ? CryptographicBuffer.DecodeFromHexString(publicKey)
-                : CryptographicBuffer.DecodeFromBase64String(publicKey);
+                ? CryptographicBuffer.DecodeFromHexString(publicOrPrivateKey)
+                : CryptographicBuffer.DecodeFromBase64String(publicOrPrivateKey);
 
             key = AsymmetricKeyAlgorithmProvider.OpenAlgorithm(asymmetricAlgorithm.ToString())
                 .ImportPublicKey(keyBlob);
@@ -23,143 +27,121 @@ public static partial class SecurityHelper
         else
         {
             key = AsymmetricKeyAlgorithmProvider.OpenAlgorithm(asymmetricAlgorithm.ToString())
-                .ImportPublicKey(publicKeyBuffer);
+                .ImportPublicKey(publicOrPrivateKeyBuffer);
         }
 
-        IBuffer encryptedData = CryptographicEngine.Encrypt(key, keyBuffer, null);
+        var encryptedData = cryptoMode == CryptoMode.Encrypt
+            ? CryptographicEngine.Encrypt(key, keyBuffer, null)
+            : CryptographicEngine.Decrypt(key, keyBuffer, null);
 
-        return encodeType == EncodeType.Hex
-            ? CryptographicBuffer.EncodeToHexString(encryptedData)
-            : CryptographicBuffer.EncodeToBase64String(encryptedData);
+        return cryptoMode == CryptoMode.Encrypt
+            ? encodeType == EncodeType.Hex
+           ? CryptographicBuffer.EncodeToHexString(encryptedData)
+           : CryptographicBuffer.EncodeToBase64String(encryptedData)
+            : CryptographicBuffer.ConvertBinaryToString(BinaryStringEncoding.Utf8, encryptedData);
+
     }
 
     public static string EncryptStringAsymmetric(string plainText, string publicKey)
     {
-        return EncryptStringAsymmetricBase(plainText, publicKey, null, AsymmetricAlgorithm.RSA_PKCS1, EncodeType.Hex);
+        return CryptoStringAsymmetricBase(CryptoMode.Encrypt, plainText, publicKey, null, AsymmetricAlgorithm.RSA_PKCS1, EncodeType.Hex);
     }
 
     public static string EncryptStringAsymmetric(string plainText, out CryptographicKey keyPair)
     {
         keyPair = GenerateAsymmetricKeyPair(AsymmetricAlgorithm.RSA_PKCS1);
-        return EncryptStringAsymmetricBase(plainText, null, keyPair.ExportPublicKey(), AsymmetricAlgorithm.RSA_PKCS1, EncodeType.Hex);
+        return CryptoStringAsymmetricBase(CryptoMode.Encrypt, plainText, null, keyPair.ExportPublicKey(), AsymmetricAlgorithm.RSA_PKCS1, EncodeType.Hex);
     }
 
     public static string EncryptStringAsymmetric(string plainText, string publicKey, AsymmetricAlgorithm asymmetricAlgorithm)
     {
-        return EncryptStringAsymmetricBase(plainText, publicKey, null, asymmetricAlgorithm, EncodeType.Hex);
+        return CryptoStringAsymmetricBase(CryptoMode.Encrypt, plainText, publicKey, null, asymmetricAlgorithm, EncodeType.Hex);
     }
     public static string EncryptStringAsymmetric(string plainText, out CryptographicKey keyPair, AsymmetricAlgorithm asymmetricAlgorithm)
     {
         keyPair = GenerateAsymmetricKeyPair(asymmetricAlgorithm);
-        return EncryptStringAsymmetricBase(plainText, null, keyPair.ExportPublicKey(), asymmetricAlgorithm, EncodeType.Hex);
+        return CryptoStringAsymmetricBase(CryptoMode.Encrypt, plainText, null, keyPair.ExportPublicKey(), asymmetricAlgorithm, EncodeType.Hex);
     }
     public static string EncryptStringAsymmetric(string plainText, string publicKey, EncodeType encodeType)
     {
-        return EncryptStringAsymmetricBase(plainText, publicKey, null, AsymmetricAlgorithm.RSA_PKCS1, encodeType);
+        return CryptoStringAsymmetricBase(CryptoMode.Encrypt, plainText, publicKey, null, AsymmetricAlgorithm.RSA_PKCS1, encodeType);
     }
 
     public static string EncryptStringAsymmetric(string plainText, out CryptographicKey keyPair, EncodeType encodeType)
     {
         keyPair = GenerateAsymmetricKeyPair(AsymmetricAlgorithm.RSA_PKCS1);
-        return EncryptStringAsymmetricBase(plainText, null, keyPair.ExportPublicKey(), AsymmetricAlgorithm.RSA_PKCS1, encodeType);
+        return CryptoStringAsymmetricBase(CryptoMode.Encrypt, plainText, null, keyPair.ExportPublicKey(), AsymmetricAlgorithm.RSA_PKCS1, encodeType);
     }
 
     public static string EncryptStringAsymmetric(string plainText, string publicKey, AsymmetricAlgorithm asymmetricAlgorithm, EncodeType encodeType)
     {
-        return EncryptStringAsymmetricBase(plainText, publicKey, null, asymmetricAlgorithm, encodeType);
+        return CryptoStringAsymmetricBase(CryptoMode.Encrypt, plainText, publicKey, null, asymmetricAlgorithm, encodeType);
     }
 
     public static string EncryptStringAsymmetric(string plainText, out CryptographicKey keyPair, AsymmetricAlgorithm asymmetricAlgorithm, EncodeType encodeType)
     {
         keyPair = GenerateAsymmetricKeyPair(asymmetricAlgorithm);
-        return EncryptStringAsymmetricBase(plainText, null, keyPair.ExportPublicKey(), asymmetricAlgorithm, encodeType);
+        return CryptoStringAsymmetricBase(CryptoMode.Encrypt, plainText, null, keyPair.ExportPublicKey(), asymmetricAlgorithm, encodeType);
     }
 
     public static string EncryptStringAsymmetric(string plainText, IBuffer publicKey)
     {
-        return EncryptStringAsymmetricBase(plainText, null, publicKey, AsymmetricAlgorithm.RSA_PKCS1, EncodeType.Hex);
+        return CryptoStringAsymmetricBase(CryptoMode.Encrypt, plainText, null, publicKey, AsymmetricAlgorithm.RSA_PKCS1, EncodeType.Hex);
     }
 
     public static string EncryptStringAsymmetric(string plainText, IBuffer publicKey, AsymmetricAlgorithm asymmetricAlgorithm)
     {
-        return EncryptStringAsymmetricBase(plainText, null, publicKey, asymmetricAlgorithm, EncodeType.Hex);
+        return CryptoStringAsymmetricBase(CryptoMode.Encrypt, plainText, null, publicKey, asymmetricAlgorithm, EncodeType.Hex);
     }
 
     public static string EncryptStringAsymmetric(string plainText, IBuffer publicKey, EncodeType encodeType)
     {
-        return EncryptStringAsymmetricBase(plainText, null, publicKey, AsymmetricAlgorithm.RSA_PKCS1, encodeType);
+        return CryptoStringAsymmetricBase(CryptoMode.Encrypt, plainText, null, publicKey, AsymmetricAlgorithm.RSA_PKCS1, encodeType);
     }
 
     public static string EncryptStringAsymmetric(string plainText, IBuffer publicKey, AsymmetricAlgorithm asymmetricAlgorithm, EncodeType encodeType)
     {
-        return EncryptStringAsymmetricBase(plainText, null, publicKey, asymmetricAlgorithm, encodeType);
-    }
-
-    private static string DecryptStringAsymmetricBase(string encryptedString, string privateKey, IBuffer privateKeyBuffer, AsymmetricAlgorithm asymmetricAlgorithm, EncodeType encodeType)
-    {
-        var keyBuffer = encodeType == EncodeType.Hex
-            ? CryptographicBuffer.DecodeFromHexString(encryptedString)
-            : CryptographicBuffer.DecodeFromBase64String(encryptedString);
-
-        CryptographicKey key;
-        if (privateKeyBuffer == null)
-        {
-            var keyBlob = encodeType == EncodeType.Hex
-                ? CryptographicBuffer.DecodeFromHexString(privateKey)
-                : CryptographicBuffer.DecodeFromBase64String(privateKey);
-
-            key = AsymmetricKeyAlgorithmProvider.OpenAlgorithm(asymmetricAlgorithm.ToString())
-                .ImportKeyPair(keyBlob);
-        }
-        else
-        {
-            key = AsymmetricKeyAlgorithmProvider.OpenAlgorithm(asymmetricAlgorithm.ToString())
-                .ImportKeyPair(privateKeyBuffer);
-        }
-
-        IBuffer decryptedData = CryptographicEngine.Decrypt(key, keyBuffer, null);
-
-        return CryptographicBuffer.ConvertBinaryToString(BinaryStringEncoding.Utf8, decryptedData);
+        return CryptoStringAsymmetricBase(CryptoMode.Encrypt, plainText, null, publicKey, asymmetricAlgorithm, encodeType);
     }
 
     public static string DecryptStringAsymmetric(string encryptedString, IBuffer privateKey)
     {
-        return DecryptStringAsymmetricBase(encryptedString, null, privateKey, AsymmetricAlgorithm.RSA_PKCS1, EncodeType.Hex);
+        return CryptoStringAsymmetricBase(CryptoMode.Decrypt, encryptedString, null, privateKey, AsymmetricAlgorithm.RSA_PKCS1, EncodeType.Hex);
     }
 
     public static string DecryptStringAsymmetric(string encryptedString, IBuffer privateKey, AsymmetricAlgorithm asymmetricAlgorithm)
     {
-        return DecryptStringAsymmetricBase(encryptedString, null, privateKey, asymmetricAlgorithm, EncodeType.Hex);
+        return CryptoStringAsymmetricBase(CryptoMode.Decrypt, encryptedString, null, privateKey, asymmetricAlgorithm, EncodeType.Hex);
     }
 
     public static string DecryptStringAsymmetric(string encryptedString, IBuffer privateKey, EncodeType encodeType)
     {
-        return DecryptStringAsymmetricBase(encryptedString, null, privateKey, AsymmetricAlgorithm.RSA_PKCS1, encodeType);
+        return CryptoStringAsymmetricBase(CryptoMode.Decrypt, encryptedString, null, privateKey, AsymmetricAlgorithm.RSA_PKCS1, encodeType);
     }
 
     public static string DecryptStringAsymmetric(string encryptedString, IBuffer privateKey, AsymmetricAlgorithm asymmetricAlgorithm, EncodeType encodeType)
     {
-        return DecryptStringAsymmetricBase(encryptedString, null, privateKey, asymmetricAlgorithm, encodeType);
+        return CryptoStringAsymmetricBase(CryptoMode.Decrypt, encryptedString, null, privateKey, asymmetricAlgorithm, encodeType);
     }
 
     public static string DecryptStringAsymmetric(string encryptedString, string privateKey)
     {
-        return DecryptStringAsymmetricBase(encryptedString, privateKey, null, AsymmetricAlgorithm.RSA_PKCS1, EncodeType.Hex);
+        return CryptoStringAsymmetricBase(CryptoMode.Decrypt, encryptedString, privateKey, null, AsymmetricAlgorithm.RSA_PKCS1, EncodeType.Hex);
     }
 
     public static string DecryptStringAsymmetric(string encryptedString, string privateKey, AsymmetricAlgorithm asymmetricAlgorithm)
     {
-        return DecryptStringAsymmetricBase(encryptedString, privateKey, null, asymmetricAlgorithm, EncodeType.Hex);
+        return CryptoStringAsymmetricBase(CryptoMode.Decrypt, encryptedString, privateKey, null, asymmetricAlgorithm, EncodeType.Hex);
     }
 
     public static string DecryptStringAsymmetric(string encryptedString, string privateKey, EncodeType encodeType)
     {
-        return DecryptStringAsymmetricBase(encryptedString, privateKey, null, AsymmetricAlgorithm.RSA_PKCS1, encodeType);
+        return CryptoStringAsymmetricBase(CryptoMode.Decrypt, encryptedString, privateKey, null, AsymmetricAlgorithm.RSA_PKCS1, encodeType);
     }
 
     public static string DecryptStringAsymmetric(string encryptedString, string privateKey, AsymmetricAlgorithm asymmetricAlgorithm, EncodeType encodeType)
     {
-        return DecryptStringAsymmetricBase(encryptedString, privateKey, null, asymmetricAlgorithm, encodeType);
+        return CryptoStringAsymmetricBase(CryptoMode.Decrypt, encryptedString, privateKey, null, asymmetricAlgorithm, encodeType);
     }
 
     private static CryptographicKey GenerateAsymmetricKeyPairBase(AsymmetricAlgorithm asymmetricAlgorithm, RSAKeySize keySize)
