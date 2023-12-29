e722b3bb2c8341a3229132c7535aa0afe04d32ca
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-12-29 15:11:34 +0330

    Update SecurityHelper.cs

--- dev/Core/Helpers/SecurityHelper/SecurityHelper.cs
+++ dev/Core/Helpers/SecurityHelper/SecurityHelper.cs
@@ -77,45 +77,4 @@ public static partial class SecurityHelper
         var btArray = Convert.FromBase64String(encryptedString);
         return Encoding.UTF8.GetString(btArray);
     }
-
-
-    [Obsolete("Use new GenerateHash method")]
-    public static string GetMD5Hash(string strMsg)
-    {
-        string strAlgName = HashAlgorithmNames.Md5;
-        IBuffer buffUtf8Msg = CryptographicBuffer.ConvertStringToBinary(strMsg, BinaryStringEncoding.Utf8);
-
-        HashAlgorithmProvider objAlgProv = HashAlgorithmProvider.OpenAlgorithm(strAlgName);
-
-        IBuffer buffHash = objAlgProv.HashData(buffUtf8Msg);
-
-        if (buffHash.Length != objAlgProv.HashLength)
-        {
-            throw new Exception("There was an error creating the hash");
-        }
-
-        string hex = CryptographicBuffer.EncodeToHexString(buffHash);
-
-        return hex;
-    }
-
-    [Obsolete("Use new GenerateHash method")]
-    public static string GetSHA256Hash(string strMsg)
-    {
-        string strAlgName = HashAlgorithmNames.Sha256;
-        IBuffer buffUtf8Msg = CryptographicBuffer.ConvertStringToBinary(strMsg, BinaryStringEncoding.Utf8);
-
-        HashAlgorithmProvider objAlgProv = HashAlgorithmProvider.OpenAlgorithm(strAlgName);
-
-        IBuffer buffHash = objAlgProv.HashData(buffUtf8Msg);
-
-        if (buffHash.Length != objAlgProv.HashLength)
-        {
-            throw new Exception("There was an error creating the hash");
-        }
-
-        string hex = CryptographicBuffer.EncodeToHexString(buffHash);
-
-        return hex;
-    }
 }
