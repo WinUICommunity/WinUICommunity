14ed7561158d2a5dfda4a8ea24132c0a37762458
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-01-08 17:33:09 +0330

    Add CopyButton

---
+++ dev/Components/Controls/Ported/CopyButton/CopyButton.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml.Media.Animation;
+
+namespace WinUICommunity;
+public sealed class CopyButton : Button
+{
+    public CopyButton()
+    {
+        this.DefaultStyleKey = typeof(CopyButton);
+    }
+
+    private void CopyButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (GetTemplateChild("CopyToClipboardSuccessAnimation") is Storyboard _storyBoard)
+        {
+            _storyBoard.Begin();
+        }
+    }
+
+    protected override void OnApplyTemplate()
+    {
+        Click -= CopyButton_Click;
+        base.OnApplyTemplate();
+        Click += CopyButton_Click;
+    }
+}
+
