05bbbb17554a5c8f62e0a648583c7bbc456b85f5
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-01-14 16:51:14 +0330

    Add Windows 8 ProgressRing Effect

---
+++ dev/Components/Controls/ProgressRing/ProgressRing.cs
@@ -0,0 +1,72 @@
+﻿using Windows.Foundation;
+
+namespace WinUICommunity;
+
+[TemplateVisualState(GroupName = GroupActive, Name = StateActive)]
+[TemplateVisualState(GroupName = GroupActive, Name = StateInactive)]
+public partial class ProgressRing : Control
+{
+    public const string GroupActive = "ActiveStates";
+    public const string StateActive = "Active";
+    public const string StateInactive = "Inactive";
+
+    public static readonly DependencyProperty IsActiveProperty =
+        DependencyProperty.Register("IsActive", typeof(bool), typeof(ProgressRing),
+            new PropertyMetadata(false, IsActiveChanged));
+
+    public static readonly DependencyProperty TemplateSettingsProperty =
+        DependencyProperty.Register("TemplateSettings", typeof(TemplateSettingValues), typeof(ProgressRing),
+            new PropertyMetadata(null));
+
+    private bool _hasAppliedTemplate;
+
+    public ProgressRing()
+    {
+        DefaultStyleKey = typeof(ProgressRing);
+        TemplateSettings = new TemplateSettingValues(60);
+    }
+
+    public bool IsActive
+    {
+        get => (bool)GetValue(IsActiveProperty);
+        set => SetValue(IsActiveProperty, value);
+    }
+
+    public TemplateSettingValues TemplateSettings
+    {
+        get => (TemplateSettingValues)GetValue(TemplateSettingsProperty);
+        set => SetValue(TemplateSettingsProperty, value);
+    }
+
+    protected override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+        _hasAppliedTemplate = true;
+        UpdateState(IsActive);
+    }
+
+    protected override Size MeasureOverride(Size availableSize)
+    {
+        var width = double.IsNaN(Width) == false ? Width : availableSize.Width;
+        var height = double.IsNaN(Height) == false ? Height : availableSize.Height;
+
+        TemplateSettings = new TemplateSettingValues(Math.Min(width, height));
+        return base.MeasureOverride(availableSize);
+    }
+
+    private static void IsActiveChanged(DependencyObject d, DependencyPropertyChangedEventArgs args)
+    {
+        var pr = (ProgressRing)d;
+        var isActive = (bool)args.NewValue;
+        pr.UpdateState(isActive);
+    }
+
+    private void UpdateState(bool isActive)
+    {
+        if (_hasAppliedTemplate)
+        {
+            var state = isActive ? StateActive : StateInactive;
+            VisualStateManager.GoToState(this, state, true);
+        }
+    }
+}
---
+++ dev/Components/Controls/ProgressRing/TemplateSettingValues.cs
@@ -0,0 +1,52 @@
+﻿namespace WinUICommunity;
+
+public partial class ProgressRing
+{
+    public class TemplateSettingValues : DependencyObject
+    {
+        public static readonly DependencyProperty EllipseDiameterProperty =
+            DependencyProperty.Register("EllipseDiameter", typeof(double), typeof(TemplateSettingValues),
+                new PropertyMetadata(0D));
+
+        public static readonly DependencyProperty EllipseOffsetProperty =
+            DependencyProperty.Register("EllipseOffset", typeof(Thickness), typeof(TemplateSettingValues),
+                new PropertyMetadata(default(Thickness)));
+
+        public static readonly DependencyProperty MaxSideLengthProperty =
+            DependencyProperty.Register("MaxSideLength", typeof(double), typeof(TemplateSettingValues),
+                new PropertyMetadata(0D));
+
+        public TemplateSettingValues(double width)
+        {
+            if (width <= 40)
+            {
+                EllipseDiameter = width / 10 + 1;
+            }
+            else
+            {
+                EllipseDiameter = width / 10;
+            }
+
+            MaxSideLength = width - EllipseDiameter;
+            EllipseOffset = new Thickness(0, EllipseDiameter * 2.5, 0, 0);
+        }
+
+        public double EllipseDiameter
+        {
+            get => (double)GetValue(EllipseDiameterProperty);
+            set => SetValue(EllipseDiameterProperty, value);
+        }
+
+        public Thickness EllipseOffset
+        {
+            get => (Thickness)GetValue(EllipseOffsetProperty);
+            set => SetValue(EllipseOffsetProperty, value);
+        }
+
+        public double MaxSideLength
+        {
+            get => (double)GetValue(MaxSideLengthProperty);
+            set => SetValue(MaxSideLengthProperty, value);
+        }
+    }
+}
