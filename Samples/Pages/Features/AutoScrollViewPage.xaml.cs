5e3eaf27da65bcbcc39f0e3e26a0be16d5cd8cb1
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-01-16 20:17:30 +0330

    Add AutoScrollView, ProgressBar Demo

---
+++ Samples/Pages/Features/AutoScrollViewPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace WinUICommunity.DemoApp.Pages;
+public sealed partial class AutoScrollViewPage : Page
+{
+    public AutoScrollViewPage()
+    {
+        this.InitializeComponent();
+    }
+
+    private void AutoScrollHoverEffectView_PointerCanceled(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+    {
+        AutoScrollHoverEffectView.IsPlaying = false;
+    }
+
+    private void AutoScrollHoverEffectView_PointerEntered(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+    {
+        AutoScrollHoverEffectView.IsPlaying = true;
+    }
+
+    private void AutoScrollHoverEffectView_PointerExited(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+    {
+        AutoScrollHoverEffectView.IsPlaying = false;
+    }
+}
