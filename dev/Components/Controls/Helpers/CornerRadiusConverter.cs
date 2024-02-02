acb0e62358533c192154743d8ef1aa54b773eb83
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-02-02 10:37:18 +0330

    [SettingsExpander] Fix CornerRadius bug

---
+++ dev/Components/Controls/Helpers/CornerRadiusConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace WinUICommunity;
+internal class CornerRadiusConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is CornerRadius cornerRadius)
+        {
+            return new CornerRadius(0, 0, cornerRadius.BottomRight, cornerRadius.BottomLeft);
+        }
+        else
+        {
+            return value;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        return value;
+    }
+}
