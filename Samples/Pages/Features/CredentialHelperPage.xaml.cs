26783ea78f636be1862f25567e73556f0112b506
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-02-05 13:07:09 +0330

    Add CredentialHelperPage Demo

--- Samples/Pages/Features/CredentialHelperPage.xaml.cs
+++ Samples/Pages/Features/CredentialHelperPage.xaml.cs
@@ -8,4 +8,32 @@ public sealed partial class CredentialHelperPage : Page
     {
         this.InitializeComponent();
     }
+
+    private async void btnRequestOSPin_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        var result = await CredentialHelper.RequestWindowsPIN(txtRequestOSPin.Text);
+        txtRequestOSPinResult.Text = result.ToString();
+    }
+
+    private async void btnPickCredential_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        var result = await CredentialHelper.PickCredential(txtPickCredentialCaption.Text, txtPickCredentialMessage.Text);
+        txtPickCredentialResult.Text = $"Username: {result.CredentialUserName} | Password: {result.CredentialPassword}";
+    }
+
+    private void btnAddPassword_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        CredentialHelper.AddPasswordCredential(txtResource.Text, txtUsername.Text, txtPassword.Text);
+    }
+
+    private void btnGetPassword_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        var result = CredentialHelper.GetPasswordCredential(txtResource.Text, txtUsername.Text);
+        txtGetPasswordResult.Text = $"Resource: {result?.Resource} | Username: {result?.UserName} | Password: {result?.UserName}";
+    }
+
+    private void btnRemovePassword_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        CredentialHelper.RemovePasswordCredential(txtResource.Text, txtUsername.Text);
+    }
 }
