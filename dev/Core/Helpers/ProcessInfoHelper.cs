84422b78ea09128d433e48c38820984476875735
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-07-29 15:50:30 +0330

    Create ProcessInfoHelper.cs

---
+++ dev/Core/Helpers/ProcessInfoHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace WinUICommunity;
+public static class ProcessInfoHelper
+{
+    public static string GetVersionString => GetVersion()?.ToString();
+    public static Version GetVersion()
+    {
+        using var process = Process.GetCurrentProcess();
+        var ver = process.MainModule.FileVersionInfo;
+        var version = new Version(ver.FileMajorPart, ver.FileMinorPart, ver.FileBuildPart, ver.FilePrivatePart);
+        return version;
+    }
+    public static FileVersionInfo GetFileVersionInfo()
+    {
+        using var process = Process.GetCurrentProcess();
+        return process.MainModule.FileVersionInfo;
+    }
+
+    public static string GetProductName()
+    {
+        using var process = Process.GetCurrentProcess();
+        return process.MainModule.FileVersionInfo.ProductName;
+    }
+}
