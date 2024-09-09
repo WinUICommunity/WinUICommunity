f9d16c2ea5e12cdd8219b9084dfdd808e6febfab
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-09-09 18:42:07 +0330

    Add ShowWindowFlagEnum

---
+++ dev/ContextMenuExtensions/ContextMenuExtensions/ShowWindowFlagEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace WinUICommunity;
+public enum ShowWindowFlagEnum
+{
+    [DescriptionAttribute("Hide")]
+    HIDE = 0,
+    [DescriptionAttribute("Show Normal")]
+    SHOWNORMAL = 1,
+    [DescriptionAttribute("Show Minimized")]
+    SHOWMINIMIZED = 2,
+    [DescriptionAttribute("Show Maximized")]
+    SHOWMAXIMIZED = 3,
+}
