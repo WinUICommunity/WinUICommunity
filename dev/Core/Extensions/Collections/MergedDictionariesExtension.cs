11dd10efdf4665a1134edaa94ed580dd49253f72
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-10-20 18:54:24 +0330

    Update MergedDictionariesExtension.cs

--- dev/Core/Extensions/Collections/MergedDictionariesExtension.cs
+++ dev/Core/Extensions/Collections/MergedDictionariesExtension.cs
@@ -51,6 +51,14 @@ public static partial class MergedDictionariesExtension
         }
     }
 
+    public static void AddIfNotExists(this Collection<ResourceDictionary> resourceDictionaries, ResourceDictionary item)
+    {
+        if (!resourceDictionaries.Any(d => d.Source == item.Source))
+        {
+            resourceDictionaries.Add(item);
+        }
+    }
+
     public static void Swap(this Collection<ResourceDictionary> mergedDictionaries, int index1, int index2)
     {
         if (index1 == index2)
@@ -63,5 +71,74 @@ public static partial class MergedDictionariesExtension
         var tmp = mergedDictionaries[smallIndex];
         mergedDictionaries.RemoveAt(smallIndex);
         mergedDictionaries.Insert(largeIndex, tmp);
+    }
+
+    public static void AddIfNotNull(this IList<ResourceDictionary> mergedDictionaries, ResourceDictionary item)
+    {
+        if (item != null)
+        {
+            mergedDictionaries.Add(item);
+        }
+    }
+
+    public static void RemoveIfNotNull(this IList<ResourceDictionary> mergedDictionaries, ResourceDictionary item)
+    {
+        if (item != null)
+        {
+            mergedDictionaries.Remove(item);
+        }
+    }
+
+    public static void InsertOrReplace(this IList<ResourceDictionary> mergedDictionaries, int index, ResourceDictionary item)
+    {
+        if (mergedDictionaries.Count > index)
+        {
+            mergedDictionaries[index] = item;
+        }
+        else
+        {
+            mergedDictionaries.Insert(index, item);
+        }
+    }
+
+    public static void RemoveAll<T>(this IList<ResourceDictionary> mergedDictionaries) where T : ResourceDictionary
+    {
+        for (int i = mergedDictionaries.Count - 1; i >= 0; i--)
+        {
+            if (mergedDictionaries[i] is T)
+            {
+                mergedDictionaries.RemoveAt(i);
+            }
+        }
+    }
+
+    public static void InsertIfNotExists(this IList<ResourceDictionary> mergedDictionaries, int index, ResourceDictionary item)
+    {
+        if (!mergedDictionaries.Contains(item))
+        {
+            mergedDictionaries.Insert(index, item);
+        }
+    }
+
+    public static void Swap(this IList<ResourceDictionary> mergedDictionaries, int index1, int index2)
+    {
+        if (index1 == index2)
+        {
+            return;
+        }
+
+        var smallIndex = Math.Min(index1, index2);
+        var largeIndex = Math.Max(index1, index2);
+        var tmp = mergedDictionaries[smallIndex];
+        mergedDictionaries.RemoveAt(smallIndex);
+        mergedDictionaries.Insert(largeIndex, tmp);
+    }
+
+    public static void AddIfNotExists(this IList<ResourceDictionary> resourceDictionaries, ResourceDictionary item)
+    {
+        if (!resourceDictionaries.Any(d => d.Source == item.Source))
+        {
+            resourceDictionaries.Add(item);
+        }
     }
 }
