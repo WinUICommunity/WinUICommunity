311f7ff38a4f8fad42c2866c38d046f9bcfbb749
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-10-20 20:50:56 +0330

    Fix MainLandingPage

--- dev/WucGalleryApp/Views/Pages/LandingsPage/HomeLandingsPage.xaml.cs
+++ dev/WucGalleryApp/Views/Pages/LandingsPage/HomeLandingsPage.xaml.cs
@@ -6,16 +6,4 @@ public sealed partial class HomeLandingsPage : Page
     {
         this.InitializeComponent();
     }
-
-    private void mainLandingsPage_Loaded(object sender, RoutedEventArgs e)
-    {
-        mainLandingsPage.GetData(App.Current.GetJsonNavigationViewService.DataSource);
-    }
-
-    private void mainLandingsPage_OnItemClick(object sender, RoutedEventArgs e)
-    {
-        var args = (ItemClickEventArgs)e;
-        var item = (DataItem)args.ClickedItem;
-        App.Current.GetJsonNavigationViewService.NavigateTo(item.UniqueId);
-    }
 }
