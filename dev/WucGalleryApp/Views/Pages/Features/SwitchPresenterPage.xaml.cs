a813b5e74d897c6b182082d80b7bcb62dde6224f
Mahdi Hosseini <mahdidvb72@gmail.com>
2024-11-09 13:59:55 +0330

    Update SwithPresenter

--- dev/WucGalleryApp/Views/Pages/Features/SwitchPresenterPage.xaml.cs
+++ dev/WucGalleryApp/Views/Pages/Features/SwitchPresenterPage.xaml.cs
@@ -9,10 +9,27 @@ public sealed partial class SwitchPresenterPage : Page
 
 public enum Animal
 {
+    Bunny,
     Cat,
     Dog,
-    Bunny,
+    Giraffe,
     Llama,
+    Otter,
+    Owl,
     Parrot,
     Squirrel
+}
+public enum CheckStatus
+{
+    Error,
+    Warning,
+    Success,
+}
+public partial class TemplateInformation
+{
+    public string? Header { get; set; }
+
+    public string? Regex { get; set; }
+
+    public string? PlaceholderText { get; set; }
 }
